4417d35200702d594b3eb3aad952dafeb189ff05
ciscoo <cisco21c@gmail.com>
2017-03-30 22:29:03 -0500

    Switch to nunjucks; Implement main layout/dashboard; Stub modals

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express')
+const expressNunjucks = require('express-nunjucks')
 const { info } = require('console')
 const path = require('path')
-const dashboardRoutes = require('./app/routes/dashboard')
+const routes = require('./app/routes')
 
 /**
  * Configure Express.
  */
 const app = express()
+const isDev = app.get('env') === 'development'
+
 app.use(express.static('app/public'))
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'app/views'))
-app.set('view engine', 'pug')
+app.set('view engine', 'njk')
+
+app.locals.defaultTitle = 'Express'
+app.locals.appName = 'Farmers Market'
+
+expressNunjucks(app, {
+  watch: isDev,
+  noCache: isDev
+})
 
 /**
  * Configure routes; all routes use Express' Router.
  */
-app.use('/dashboard', dashboardRoutes)
+app.use('/dashboard', routes.dashboard)
+app.use('/events', routes.events)
 
 app.listen(app.get('port'), () => {
   info(`🚀 App is running on port ${app.get('port')} in ${app.get('env')} mode.`)
