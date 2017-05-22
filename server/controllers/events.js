c03b7e4f8ceeb1a9021fe47b2c34ef7031266845
ciscoo <cisco21c@gmail.com>
2017-05-21 23:06:34 -0500

    Implement events.index to list all events for a google calendar.

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,4 +1,27 @@
-exports.index = (req, res) => {}
+const google = require('googleapis')
+const auth = require('../google-apis')
+const Promise = require('bluebird')
+
+const events = google.calendar('v3').events
+const calendarId = '269m08gnggsctv40qtn2kpclgs@group.calendar.google.com'
+
+exports.index = (req, res) => {
+  const options = {
+    auth,
+    calendarId
+  }
+  Promise.fromCallback(cb => events.list(options, cb))
+    .then(({ items }) => {
+      res.json(items)
+    })
+    .error(error => {
+      const response = {
+        code: error.code,
+        message: error.errors[0].message
+      }
+      res.status(error.code).json(response)
+    })
+}
 
 exports.create = (req, res) => {}
 
