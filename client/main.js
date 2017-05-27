d24f4db3aa29d5bba8626aa3aaa2a44176abb936
ciscoo <cisco21c@gmail.com>
2017-05-27 13:41:32 -0500

    Switch to Vuetify as it's more complete and stable.

--- client/main.js
+++ client/main.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import BootstrapVue from 'bootstrap-vue'
 import { router } from './router'
 import App from './App.vue'
 import store from './store'
 import { sync } from 'vuex-router-sync'
 import { USER_LOGIN } from './store/mutationTypes'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import * as firebase from 'firebase'
 import { config as firebaseConfig } from './firebase'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 sync(store, router)
 
 firebase.initializeApp(firebaseConfig)
 
 Vue.use(VueRouter)
-Vue.use(BootstrapVue)
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
