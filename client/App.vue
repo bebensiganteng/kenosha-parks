fff11c7d67963a89e2cd38e73a875512f6de4167
ciscoo <cisco21c@gmail.com>
2017-05-27 22:09:53 -0500

    Overhaul general layout.

--- client/App.vue
+++ client/App.vue
@@ -1,3 +1,78 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <v-navigation-drawer v-if="this.$store.getters.authenticated" persistent v-model="drawer" light>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-list-item v-if="!item.divider">
+            <v-list-tile :to="item.to ? item.to : null">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-item>
+          <v-divider dark v-else="item.divider" class="my-4" />
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar v-if="this.$store.getters.authenticated" class="teal">
+      <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Kenosha Farmers' Market &mdash; {{ pageTitle }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn light icon @click.native.stop="logout">
+        <v-icon>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <transition name="slide">
+          <router-view />
+        </transition>
+      </v-container>
+    </main> 
+    <v-footer v-if="this.$store.getters.authenticated" class="teal">
+      <span>© 2017</span>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions([
+      'logout'
+    ])
+  },
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { icon: 'announcement', text: 'Notifications', to: '/notifications' },
+        { icon: 'event', text: 'Events', to: '/events' },
+        { icon: 'local_florist', text: 'Markets', to: '/markets' },
+        { divider: true },
+        { icon: 'bug_report', text: 'Report bug', to: '/issues' },
+        { icon: 'info', text: 'Info', to: '/info' }
+      ]
+    }
+  },
+  computed: {
+    pageTitle: {
+      get () {
+        return this.$store.state.pageTitle
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
