44d079f58d40c153b0d8209858a433a26b5d1bb4
ciscoo <cisco21c@gmail.com>
2017-06-09 12:15:56 -0500

    Add route to get list of all events from all calendars, requested by Alex.

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -25,6 +25,30 @@ exports.destroy = ({ query, params }, res) => {
   createAPIRequest(createOptions(query, params.calendarId), 'delete', res)
 }
 
+exports.all = async ({ query }, res) => {
+  // These are hard coded since it's a small set.
+  // Doing requests dynamically got really messy, really fast.
+  const ids = [
+    'parksalliancecalendar@gmail.com',
+    '2ok3o3k6kugcnoe0milta4kqog@group.calendar.google.com',
+    'h5hacgta0g3qte2gr9d508ne18@group.calendar.google.com'
+  ]
+  const options = ids.map(id => {
+    return createOptions(query, id)
+  })
+  try {
+    const lincoln = await Promise.fromCallback(cb => events.list(options[0], cb))
+    const hobbs = await Promise.fromCallback(cb => events.list(options[1], cb))
+    const roosevelt = await Promise.fromCallback(cb => events.list(options[2], cb))
+    let response = lincoln
+    response.items = response.items.concat(hobbs.items)
+    response.items = response.items.concat(roosevelt.items)
+    res.json(response)
+  } catch (error) {
+    res.json(error)
+  }
+}
+
 function createOptions (query, calendarId, props = {}) {
   let options = Object.assign({ auth, calendarId }, query)
   options = Object.assign(options, props)
--- server/routes/calendars.js
+++ server/routes/calendars.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { calendarController } = require('../controllers')
+const { eventsController } = require('../controllers')
 const eventsRoutes = require('./events')
 
 const router = express.Router()
 
 router.get('/:calendarId', calendarController.index)
+router.get('/all/events', eventsController.all)
 router.use('/:calendarId/events', eventsRoutes)
 
 module.exports = router
