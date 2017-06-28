4444a33b41a2b2bb7552a87a93eb2cae81602e6e
ciscoo <cisco21c@gmail.com>
2017-06-27 20:09:31 -0500

    Remove reference to notification controller.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,9 @@
 const eventsController = require('./events')
-const notificationController = require('./notification')
 const calendarController = require('./calendars')
 const filesController = require('./files')
 
 module.exports = {
   eventsController,
-  notificationController,
   calendarController,
   filesController
 }
